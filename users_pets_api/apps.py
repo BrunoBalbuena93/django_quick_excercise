8716d3d238457b31670ae9f544d99c58f9b439af
Juan Camilo Trujillo Cadavid <camilo2046@gmail.com>
2023-03-01 14:22:16 +0000

    Rename the user_pets_api to users_pets_api

--- users_pets_api/apps.py
+++ users_pets_api/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class UserPetsApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'user_pets_api'
+    name = 'users_pets_api'
